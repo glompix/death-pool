bb081011669ba55d7e7a00e37b78c3ca232a5805
Stuart Branham <glompix@gmail.com>
2016-04-30 13:28:13 -0700

    Facebook login

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,7 @@ require('normalize.css/normalize.css');
 require('styles/App.css');
 
 import React from 'react';
+import Login from './Login';
 
 let yeomanImage = require('../images/yeoman.png');
 
@@ -9,14 +10,16 @@ class AppComponent extends React.Component {
   render() {
     return (
       <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+        <Login/>
+        <img src={yeomanImage} alt="Yeoman Generator"/>
+        <div className="notice">Please edit
+          <code>src/components/Main.js</code>
+          to get started!</div>
       </div>
     );
   }
 }
 
-AppComponent.defaultProps = {
-};
+AppComponent.defaultProps = {};
 
 export default AppComponent;
